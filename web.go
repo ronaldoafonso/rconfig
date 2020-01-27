7ddc003d1a5a3c438c519da4b53170142cd3281e
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-01-27 11:59:12 -0300

    Use "Gorilla Mux" as request router.

--- web.go
+++ web.go
@@ -5,10 +5,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/ronaldoafonso/rconfig/rconfig"
 	"github.com/ronaldoafonso/rconfig/rmac"
 	"net/http"
-	"strings"
 )
 
 // Box ... An OpenWrt box struct
@@ -18,7 +18,8 @@ type Box struct {
 }
 
 var (
-	box1 = Box{
+	boxes = make(map[string]Box)
+	box1  = Box{
 		Boxname: "box1",
 		Config: rconfig.Config{
 			SSID:      "SSID1",
@@ -28,33 +29,66 @@ var (
 			},
 		},
 	}
+	box2 = Box{
+		Boxname: "box2",
+		Config: rconfig.Config{
+			SSID:      "SSID2",
+			LeaseTime: "2m",
+			AllowedMACs: rmac.AllowedMACs{
+				"22:22:22:22:22:22",
+			},
+		},
+	}
+	box3 = Box{
+		Boxname: "box3",
+		Config: rconfig.Config{
+			SSID:      "SSID3",
+			LeaseTime: "3m",
+			AllowedMACs: rmac.AllowedMACs{
+				"33:33:33:33:33:33",
+			},
+		},
+	}
 )
 
+func init() {
+	boxes["box1"] = box1
+	boxes["box2"] = box2
+	boxes["box3"] = box3
+}
+
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/box/{box}", handleWebBoxesRequests)
 	server := &http.Server{
-		Addr: ":8080",
+		Handler: router,
+		Addr:    ":8080",
 	}
-	http.HandleFunc("/", handleWebBoxesRequests)
 	fmt.Println("Ready to handle rconfig web requests ...")
 	server.ListenAndServe()
 }
 
+// showBox ... Show box struct to clients
+func showBox(w http.ResponseWriter, box *Box) {
+	fmt.Fprintf(w, "%s %s %s %v\n",
+		box.Boxname,
+		box.Config.SSID,
+		box.Config.LeaseTime,
+		box.Config.AllowedMACs)
+}
+
 // handleWebBoxesRequests ... Handle REST web boxes requests
 func handleWebBoxesRequests(w http.ResponseWriter, r *http.Request) {
-	urlPathElements := strings.Split(r.URL.Path, "/")
-	if urlPathElements[1] == "boxes" {
-		if urlPathElements[2] == "box1" {
-			fmt.Fprintf(w, "%s %s %s %v\n",
-				box1.Boxname,
-				box1.Config.SSID,
-				box1.Config.LeaseTime,
-				box1.Config.AllowedMACs)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 - Not Found"))
+	vars := mux.Vars(r)
+	boxname := vars["box"]
+	if box, ok := boxes[boxname]; ok {
+		showBox(w, &box)
+	} else if boxname == "all" {
+		for _, box := range boxes {
+			showBox(w, &box)
 		}
 	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 - Bad Request"))
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 - Not Found"))
 	}
 }
